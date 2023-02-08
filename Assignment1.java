cbe962c91899fb87460b3ff5af597d25d737ddf6
nboatey <boatey@hartford.edu>
2023-02-08 16:52:08 -0500

    Add assignment 1 work

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,32 @@
 // Import libraries here
+import java.util.Scanner;
+import java.util.Random;
+public class Assignment1 
+{
+    public static void main(String[] args) 
+    {
+        // Add your code
+      System.out.println(n:"  N N              N            BBBBBBBB")
+      System.out.println(n:"  N /t N           N            B        B") 
+      System.out.println(n:"  N /t   N         N            B        B")
+      System.out.println(n:"  N /t     N       N            BBBBBBB B")
+      System.out.println(n:"  N /t       N     N            B        B")
+      System.out.println(n:"  N/t          N   N            B         B")
+      System.out.println(n:"  N /t           N N            B        B")  
+      System.out.println(n:"  N /t             N            BBBBBBBBBB")
+     // conversion from farenheit to celsius
+      final int START_TEMP = 32;
+      final double CONVERSION_FACTOR = 9.0 / 5.0;
+      double fahrenheitTemp;
+      int celsiusTemp = 24;   // value to convert
+      fahrenheitTemp = celsiusTemp * CONVERSION_FACTOR + START_TEMP;
+      System.out.println("Celsius Temperature: " + celsiusTemp);
+      System.out.println("Fahrenheit Equivalent: " + fahrenheitTemp);
 
-public class Assignment1 {
-    public static void main(String[] args) {
-        // Add your code here
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Enter a 5 letter word:");
+      String word = scan.next();
+
+      System.out.println(s1.charAt());
     }
 }
